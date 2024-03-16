ce7e41f9534f442954c097dd475f3506e5a8e624
Jacob Chrzanowski <jlc3467@rit.edu>
2024-03-16 10:55:21 -0400

    Changed docker cleanup commands

--- .config/zsh/nginx_shortcuts.sh
+++ .config/zsh/nginx_shortcuts.sh
@@ -14,9 +14,41 @@ alias nimg2="sudo cp -r ~/nginx/images /var/www/"
 #alias docker_clean="docker container rm $(docker container ls -a | grep docker_flaskapp | grep -v prod | awk '{print $1}'); docker image rm docker_flaskapp-web"
 #alias docker_clean_prod="docker container rm $(docker container ls -a | grep docker_flaskapp_prod | awk '{print $1}'); docker image rm docker_flaskapp_prod-web"
 function docker_clean {
-    docker container stop $(docker ps -a | grep "docker_flaskapp" | awk '{print $1}')
-    docker container rm -vf $(docker ps -a | grep "docker_flaskapp" | awk '{print $1}')
-    docker image rm docker_flaskapp-web;
+    if [ "$1" = "prod" ]; then
+        prefix="inacanoe-prod"
+    elif [ "$1" = "qa" ]; then
+        prefix="inacanoe-qa"
+    else
+        echo "Invalid environment. Please specify 'prod' or 'qa'."
+        return 1
+    fi
+    # Store the output of docker ps -a in a variable
+    containers=($(docker ps -a | grep $prefix | awk '{print $1}'))
+
+    # Check if containers exist before trying to stop and remove them
+    if [ -n "$containers" ]; then
+        docker container stop "${containers[@]}";
+        docker container rm -vf "${containers[@]}";
+    else
+        echo "No containers matching prefix '$prefix' found";
+    fi
+
+    # Remove images matching the pattern 'inacanoe-qa-*'
+    images=$(docker images | awk -v term="$prefix" '$1 ~ term"-" {print $3}')
+    if [ -n "$images" ]; then
+        docker image rm "${images[@]}";
+    else
+        echo "No images matching prefix '$prefix' found.";
+    fi
+}
+function docker_clean2 {
+    docker container rm $(docker container ls -a | grep -i 'inacanoe-qa-web' | awk '{print $1}'); docker image rm inacanoe-qa-web;
+}
+function docker_clean_old {
+    docker container stop $(docker ps -a | grep "inacanoe-qa" | awk '{print $1}')
+    docker container rm -vf $(docker ps -a | grep "inacanoe-qa" | awk '{print $1}')
+    docker image rm inacanoe-qa-web;
+    #docker image rm inacanoe-qa-db;
 }
 function docker_clean_prod {
     docker container stop $(docker ps -a | grep "docker_flaskapp_prod" | awk '{print $1}')
