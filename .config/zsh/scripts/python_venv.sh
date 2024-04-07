bb0f1a7a81573b242eb51b6dfa0708da125afc62
Jacob Chrzanowski <jlc3467@rit.edu>
2024-04-07 13:01:10 -0400

    Added fix for python-partial-less environments. #2

--- .config/zsh/scripts/python_venv.sh
+++ .config/zsh/scripts/python_venv.sh
@@ -12,13 +12,14 @@ fi
 venv_path="$HOME/.python_venv"
 
 # Check and create if missing
-if [ -d "$venv_path" ]; then
+if [ -f "$venv_path/bin/activate" ]; then
 else
     $python -m venv "$venv_path"
     if [[ $? -eq 0 ]]; then
-        echo "Python virtual environment created successfully in $venv_path"
+        echo "Python virtual environment created successfully in $venv_path";
     else
-        echo "Python virtual environment could not be created in $venv_path"
+        echo "Python virtual environment could not be created in $venv_path";
+        exit 1;
     fi
 fi
 echo "Entering python virtual env in $venv_path"
