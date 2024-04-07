9f1373c53b33d8b18d0e5e79ebbcdb698b5a365c
Jacob Chrzanowski <jlc3467@rit.edu>
2024-04-07 13:17:58 -0400

    Another fix.

--- .config/zsh/fzf_bindings.zsh
+++ .config/zsh/fzf_bindings.zsh
@@ -4,7 +4,7 @@ if [[ ! $(which fzf) ]]; then
     return 1
 fi
 fzf_share='/usr/share/fzf'
-fzf_fallback_share='$HOME/.config/zsh/scripts/fzf'
+fzf_fallback_share="$HOME/.config/zsh/scripts/fzf"
 completion='completion.zsh'
 key_bindings='key-bindings.zsh'
 
@@ -14,17 +14,17 @@ p_key_bindings="${fzf_share}/$key_bindings"
 f_completion="${fzf_fallback_share}/${completion}"
 f_key_bindings="${fzf_fallback_share}/${key_bindings}"
 
-if [ -f $p_completion ]; then
+if [ -f "$p_completion" ]; then
     source $p_completion;
 else
     echo "FZF Completion file is missing: '$completion' using included fallback instead.";
     source $f_completion
 fi;
 
-if [ -f $p_key_bindings ]; then
+if [ -f "$p_key_bindings" ]; then
     source $p_key_bindings
 else
-    echo "FZF Key Bindings file is missing: '${key_binding}' using included fallback instead.";
+    echo "FZF Key Bindings file is missing: '${key_bindings}' using included fallback instead.";
     source $f_key_bindings
 fi;
 
