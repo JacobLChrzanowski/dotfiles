45bf56cacecbf7b92f70183fc304410f374949f5
Jacob Chrzanowski <jlc3467@rit.edu>
2024-04-07 12:57:31 -0400

    Added fix for python-partial-less environments.

--- .config/zsh/scripts/python_venv.sh
+++ .config/zsh/scripts/python_venv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+python=":"
 if [[ $(which python3) ]]; then
-    :
+    python=$(which python3);
 elif [[ $(which python) ]]; then
-    :
+    python=$(which python);
 else
     echo 'Python is not installed';
     exit 1
@@ -14,8 +14,12 @@ venv_path="$HOME/.python_venv"
 # Check and create if missing
 if [ -d "$venv_path" ]; then
 else
-    python3 -m venv "$venv_path"
-    echo "Python virtual environment created successfully in $venv_path"
+    $python -m venv "$venv_path"
+    if [[ $? -eq 0 ]]; then
+        echo "Python virtual environment created successfully in $venv_path"
+    else
+        echo "Python virtual environment could not be created in $venv_path"
+    fi
 fi
 echo "Entering python virtual env in $venv_path"
 source $venv_path/bin/activate
